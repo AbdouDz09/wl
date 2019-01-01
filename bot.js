943c51fda14a7bac200a219e1d17e2cdcc9e10aa
AbdouDz09 <45575757+AbdouDz09@users.noreply.github.com>
2019-01-01 11:42:34 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("guildMemberAdd", member => {
+        if(member.guild.id === "525580458340384768") {  
+  const channel = member.guild.channels.find('id', '528564782630961182'); 
+if (!channel) return;
+channel.send(`**<@${member.user.id}> أهلين تشرفنا`)
+}});
+
+client.login(process.env.BOT_TOKEN);
